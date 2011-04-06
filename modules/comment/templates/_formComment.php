6601aba0ed81b751cbb5deccbfa28693af0bf2ac
Morvan Jean-Philippe <jp.morvan@ville-villejuif.fr>
2011-04-06 09:47:46 +0200

    update form name to 'comment'.$form->getName() and add name attribute to form tag

--- modules/comment/templates/_formComment.php
+++ modules/comment/templates/_formComment.php
@@ -4,7 +4,7 @@
 <a name="comments-<?php echo $crypt ?>"></a>
 <div class="form-comment">
 <?php if( vjComment::checkAccessToForm($sf_user) ): ?>
-  <form action="<?php echo url_for($sf_request->getUri()) ?>" method="post">
+  <form action="<?php echo url_for($sf_request->getUri()) ?>" method="post" name="<?php echo $form->getName() ?>">
   <fieldset>
     <legend><?php echo __('Add new comment', array(), 'vjComment') ?></legend>
     <?php include_partial("comment/form", array('form' => $form)) ?>
